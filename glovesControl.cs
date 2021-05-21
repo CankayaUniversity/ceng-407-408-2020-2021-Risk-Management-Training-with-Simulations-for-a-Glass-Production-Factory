8cd13f9923493d6a0d8a5d8856e55346218c4559
Ecem Emiroglu <emirogluecemm@gmail.com>
2021-05-21 22:54:59 +0300

    Add files via upload

---
+++ glovesControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glovesControl : MonoBehaviour
+{
+
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    public Material gloves_Material;
+
+
+    private GameObject panel;
+    private void Start()
+    {
+        panel = GameObject.FindGameObjectWithTag("Panel");
+    }
+
+    public  void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("char_rightHand"))
+         {
+             rightHand.GetComponent<SkinnedMeshRenderer>().material = gloves_Material;
+             Destroy(this.gameObject);
+             panel.GetComponent<panelConroller>().isHelmet = true;
+        }
+         if(other.tag.Equals("char_leftHand"))
+         {
+             leftHand.GetComponent<SkinnedMeshRenderer>().material = gloves_Material;
+             Destroy(this.gameObject);
+             panel.GetComponent<panelConroller>().isHelmet = true;
+        }
+        
+    }
+}
---
+++ kaskController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kaskController : MonoBehaviour
+{
+
+    private GameObject panel;
+
+    private void Start()
+    {
+        panel = GameObject.FindGameObjectWithTag("Panel");
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("char_head"))
+        {
+            Destroy(this.gameObject);
+            panel.GetComponent<panelConroller>().isHelmet = true;
+        }
+    }
+}
